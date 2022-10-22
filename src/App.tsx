002599864fc7138c572097c528541abe81bc61a0
Samuel <batistasamuel1000@gmail.com>
2022-10-22 16:38:51 -0300

    teste1

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import GlobalStyles from './styles/GlobalStyles';
+import Layout from './components/layout';
+
+function App() {
+  return (
+    <>
+      <Layout />
+
+      <GlobalStyles/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/ProfilePage/index.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+
+import { 
+    Container,
+    Banner,
+    Avatar,
+    ProfileData,
+    LocationIcon,
+    CakeIcon,
+    Followage, } from './styles';
+
+const ProfilePage: React.FC = () => {
+  return (
+    <Container>
+        <Banner>
+            <Avatar />
+        </Banner>
+
+        <ProfileData>
+            {/* <EditButton outlined>Editar Perfil</EditButton> */}
+
+            <h1>Samueu</h1>
+            <h2>@samueu</h2>
+
+            <p>
+                Student and Developer <a href='https://instagram.com/samuuelbatista'>@Instagram</a>
+            </p>
+
+            <ul>
+                <li>
+                    <LocationIcon />
+                    Belo Horizonte
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <CakeIcon />
+                    Nasceu 9 de fevereiro de 2004
+                </li>
+            </ul>
+
+            <Followage>
+                <span>
+                    Seguindo <strong> 908 </strong>
+                </span>
+                <span>
+                    <strong> 1080 </strong> seguidores
+                </span>
+            </Followage>
+        </ProfileData>
+    </Container>
+
+  );
+}
+
+export default ProfilePage;
