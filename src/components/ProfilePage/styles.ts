1e4f8d95a9642cb486802997be34d3e591900d5f
Samuel <batistasamuel1000@gmail.com>
2022-11-07 14:18:03 -0300

    Perfil Profile

--- src/components/ProfilePage/styles.ts
+++ src/components/ProfilePage/styles.ts
@@ -6,28 +6,35 @@ export const Container = styled.div ``;
 export const Banner = styled.div `
     flex-shrink: 0;
 
-    width: 100%;
+    width:100%;
     height: min(33vw, 199px);
-
+    
     background: var(--twitter);
 
     position: relative;
+
 `;
 
-export const Avatar = styled.div `
-    width: max(45, min(135px , 22vw));
-    height: max(45, min(135px , 22vw));
+export const Avatar = styled.div`
+    width: max(45px, min(135px, 22vw));
+    height: max(45px, min(135px, 22vw));
 
     border: 3.75px solid var(--primary);
     background: var(--gray);
     border-radius: 50%;
-    
+
     position: absolute;
     bottom: max(-60px, -10vw);
     left: 15px;
+
 `;
 
-export const ProfileData = styled.div ``;
+export const ProfileData = styled.div `
+    padding: min(calc(10vw + 7px) , 67px) 16px 0;
+
+    display: flex;
+    flex-direction: column;
+`;
 
 const iconCSS = css`
     width: 20px;
